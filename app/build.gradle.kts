7e78133e98f8c05fd914881674be49ae1fe93bf6
JuanjoA <juanjoaguadosegovia@gmail.com>
2024-05-30 22:01:44 +0200

    Proyecto final

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,11 +41,13 @@ dependencies {
     implementation("com.google.android.material:material:1.6.0")
     implementation ("androidx.drawerlayout:drawerlayout:1.1.1")
     implementation ("androidx.appcompat:appcompat:1.2.0")
-    implementation ("com.google.firebase:firebase-auth")
-
     implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
+
+    implementation ("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-analytics")
     implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
+
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
